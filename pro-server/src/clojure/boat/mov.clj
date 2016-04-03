3ae7605154066c84b5fd7fce62434a2fd4864b5d
rururu <sorokin@oogis.ru>
2016-04-03 23:19:20 +0300

    added funcs: speed, course, coord

--- pro-server/src/clojure/boat/mov.clj
+++ pro-server/src/clojure/boat/mov.clj
@@ -7,20 +7,35 @@
 (def SPD-MAX 44)
 (def SPD-MIN -8)
 (def BOAT-TIO 1000)
-(defn helm-work [bdt]
-(letfn [(cp [crs] (if (>= crs 360) (- crs 360) crs))
-           (cm [crs] (if (< crs 0) (+ crs 360) crs))]
-  (condp = (:helm bdt)
-    :steady bdt
-    :starboard (assoc bdt :crs (cp (+ (:crs bdt) CRS-STP)))
-    :hard-starboard (assoc bdt :crs (cp (+ (:crs bdt) CRS-HRD)))
-    :port (assoc bdt :crs (cm (- (:crs bdt) CRS-STP)))
-    :hard-port (assoc bdt :crs (cm (- (:crs bdt) CRS-HRD))))))
-(defn engine-work [bdt]
-(let [old (:spd bdt)
-       new (:engine bdt)]
-  (cond
-    (= new old) bdt
-    (and (> new old) (< old SPD-MAX)) (assoc bdt :spd (+ old SPD-STP))
-    (and (< new old) (> old SPD-MIN)) (assoc bdt :spd (- old SPD-STP))
-    true bdt)))
+(defn helm
+([id cmd]
+  (vswap! BOATS assoc-in [id :helm] cmd))
+([bdt]
+  (letfn [(cp [crs] (if (>= crs 360) (- crs 360) crs))
+             (cm [crs] (if (< crs 0) (+ crs 360) crs))]
+    (condp = (:helm bdt)
+      :steady bdt
+      :starboard (assoc bdt :crs (cp (+ (:crs bdt) CRS-STP)))
+      :hard-starboard (assoc bdt :crs (cp (+ (:crs bdt) CRS-HRD)))
+      :port (assoc bdt :crs (cm (- (:crs bdt) CRS-STP)))
+      :hard-port (assoc bdt :crs (cm (- (:crs bdt) CRS-HRD)))))))
+(defn engine
+([id knots]
+  (vswap! BOATS assoc-in [id :engine] knots))
+([bdt]
+  (let [old (:spd bdt)
+         new (:engine bdt)]
+    (cond
+      (= new old) bdt
+      (and (> new old) (< old SPD-MAX)) (assoc bdt :spd (+ old SPD-STP))
+      (and (< new old) (> old SPD-MIN)) (assoc bdt :spd (- old SPD-STP))
+      true bdt))))
+(defn move [bdt]
+)
+(defn speed [id]
+(get-in @BOATS [id :spd]))
+(defn course [id]
+(get-in @BOATS [id :crs]))
+(defn coord [id]
+[(get-in @BOATS [id :lat])
+ (get-in @BOATS [id :lon])])
