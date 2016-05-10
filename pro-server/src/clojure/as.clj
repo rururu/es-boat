1bd9c75e8b2a5ded9e5a22d17aca0d9fe325798f
rururu <sorokin@oogis.ru>
2016-05-10 17:17:59 +0300

    added Answer2 with 3 accumulators

--- pro-server/src/clojure/as.clj
+++ pro-server/src/clojure/as.clj
@@ -16,3 +16,17 @@
     6 (str "Islands: " (apply str (interpose ", " (map tit-dis acc))) " are " subject)
     (str "Islands: " (apply str (interpose ", " (map tit-dis (take 5 acc)))) " and more " (- cnt 5) " islands are " subject))))
 
+(defn ans-objects [places naturals amenitys subject]
+  (letfn [(anso [acc cnt pfx]
+	(condp > cnt
+	  1 ""
+	  2 (str pfx " object " (tit-dis (first acc)) "\n")
+	  6 (str pfx " objects: " (apply str (interpose ", " (map tit-dis acc))) "\n")
+	  (str pfx " objects: " (apply str (interpose ", " (map tit-dis (take 5 acc)))) " and more " (- cnt 5) "\n")))]
+  (let [pll (sort-by second places)
+          nal (sort-by second naturals)
+          aml (sort-by second amenitys)]
+    (str (anso pll (count pll) "Place")
+          (anso nal (count nal) "Natural")
+          (anso aml (count aml) "Amenity")))))
+
